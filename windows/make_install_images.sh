f42f2017697c208f96b1952c2ea5e5659f2753b3
SATO Kentaro <kentaro@ranvis.com>
2021-07-16 20:16:39 +0900

    Use magick command for convert.

--- windows/make_install_images.sh
+++ windows/make_install_images.sh
@@ -10,13 +10,13 @@ if test -f installer.wxs -a ! -f putty.h -a -f ../putty.h; then
   cd ..
 fi
 
-convert -size 164x312 'gradient:blue-white' -distort SRT -90 -swirl 180 \
+magick convert -size 164x312 'gradient:blue-white' -distort SRT -90 -swirl 180 \
         \( icons/putty-48.png -geometry +28+24 \) -composite \
         \( icons/pscp-48.png -geometry +88+96 \) -composite \
         \( icons/puttygen-48.png -geometry +28+168 \) -composite \
         \( icons/pageant-48.png -geometry +88+240 \) -composite \
         windows/msidialog.bmp
 
-convert -size 493x58 canvas:white \
+magick convert -size 493x58 canvas:white \
         \( icons/putty-48.png -geometry +440+5 \) -composite \
         windows/msibanner.bmp
