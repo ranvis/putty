ranvis/putty

commit c7818e86e13e019e89112c286be3ff0f106ada14

Add dialog font size registry setting.
Not all dialogs are adopted.

windows/pageant.c
@@ -40,6 +40,8 @@
 
 #define APPNAME "Pageant"
 
+const char *const appname = STR(APPNAME);
+
 /* Titles and class names for invisible windows. IPCWINTITLE and
  * IPCCLASSNAME are critical to backwards compatibility: WM_COPYDATA
  * based Pageant clients will call FindWindow with those parameters
@@ -621,29 +623,6 @@ static void prompt_add_keyfile(bool encrypted)
         pageant_forget_passphrases();
     }
     sfree(filelist);
-}
-
-static int app_conf_get_int(const char *name, int def, int min, int max)
-{
-    int value = def;
-    if (get_use_inifile()) {
-        if (!get_ini_int(APPNAME, name, inifile, &value))
-            value = def;
-    } else {
-        HKEY hkey = open_regkey_ro(HKEY_CURRENT_USER, PUTTY_REG_POS "\\" APPNAME);
-        if (hkey) {
-            DWORD dw;
-            if (get_reg_dword(hkey, name, &dw))
-                value = dw;
-            else
-                value = def;
-            close_regkey(hkey);
-        }
-    }
-    if (value < min || max < value) {
-        value = def;
-    }
-    return value;
 }
 
 struct ConfirmAcceptAgentProcStruct {

windows/puttygen.c
@@ -30,6 +30,10 @@
 #undef AppendMenu
 #define AppendMenu(m, f, i, t) l10nAppendMenu(m, f, i, t)
 
+#define APPNAME "PuTTYgen"
+
+const char *const appname = STR(APPNAME);
+
 static char *cmdline_keyfile = NULL;
 static ptrlen cmdline_demo_keystr;
 static const char *demo_screenshot_filename = NULL;

windows/CMakeLists.txt
@@ -32,10 +32,11 @@ add_sources_from_current_dir(utils
   utils/screenshot.c
   utils/security.c
   utils/shinydialogbox.c
-  utils/sleep_ticks.c
   utils/split_into_argv.c
   utils/version.c
   utils/win_strerror.c
+  utils/app_conf.c
+  utils/sleep_ticks.c
   winl10n.c
 # winl10n must be used for CLI too. nol10n.c cannot query connection sharing with text translation.
   unicode.c)

windows/platform.h
@@ -606,6 +606,7 @@ void escape_registry_key(const char *in, strbuf *out);
 void escape_ini_section(const char *in, strbuf *out);
 void escape_ini_value_n(const char *in, size_t n, strbuf *out);
 void unescape_registry_key(const char *in, strbuf *out);
+int app_conf_get_int(const char *name, int def, int min, int max);
 
 bool is_console_handle(HANDLE);
 

windows/putty_sample.ini
@@ -1,6 +1,9 @@
 [Generic]
 UseIniFile=1
 
+[PuTTY]
+;DialogFontSize=0
+
 [Pageant]
 ;ConfirmAnyRequest=0
 ;ConfirmDelayMs=400

windows/utils/app_conf.c
@@ -1,0 +1,27 @@
+#include "putty.h"
+#include "ini.h"
+
+int app_conf_get_int(const char *name, int def, int min, int max)
+{
+    int value = def;
+    if (get_use_inifile()) {
+        if (!get_ini_int(appname, name, inifile, &value))
+            value = def;
+    } else {
+        char path[256];
+        snprintf(path, sizeof path, PUTTY_REG_POS "\\%s", appname);
+        HKEY hkey = open_regkey_ro(HKEY_CURRENT_USER, path);
+        if (hkey) {
+            DWORD dw;
+            if (get_reg_dword(hkey, name, &dw))
+                value = dw;
+            else
+                value = def;
+            close_regkey(hkey);
+        }
+    }
+    if (value < min || max < value) {
+        value = def;
+    }
+    return value;
+}

windows/utils/shinydialogbox.c
@@ -22,6 +22,8 @@ struct ShinyDialogBoxState {
     void *ctx;
 };
 
+static HWND CreateDialogWithFontSize(HINSTANCE hinst, LPCTSTR tmpl, HWND hwndparent, DLGPROC lpDialogFunc);
+
 /*
  * For use in re-entrant calls to the dialog procedure from
  * CreateDialog itself, temporarily store the state pointer that we
@@ -83,7 +85,7 @@ int ShinyDialogBox(HINSTANCE hinst, LPCTSTR tmpl, const char *winclass,
     state->ctx = ctx;
 
     sdb_tempstate = state;
-    HWND hwnd = CreateDialog(hinst, tmpl, hwndparent, ShinyRealDlgProc);
+    HWND hwnd = CreateDialogWithFontSize(hinst, tmpl, hwndparent, ShinyRealDlgProc);
     l10n_created_window(hwnd);
     SetWindowLongPtr(hwnd, DLGWINDOWEXTRA, (LONG_PTR)state);
     sdb_tempstate = NULL;
@@ -109,4 +111,80 @@ void ShinyEndDialog(HWND hwnd, int ret)
     struct ShinyDialogBoxState *state = ShinyDialogGetState(hwnd);
     state->result = ret;
     state->ended = true;
+}
+
+static WORD *skip_dialog_string(WORD *ptr)
+{
+    while (*ptr++) {
+        (void)0;
+    }
+    return ptr;
+}
+
+static WORD *skip_dialog_x_array(WORD *ptr)
+{
+    if (*ptr == 0xffff) {
+        return ptr + 2; // ordinal value of a menu resource
+    }
+    return skip_dialog_string(ptr);
+}
+
+// https://learn.microsoft.com/en-us/windows/win32/dlgbox/dlgtemplateex
+#pragma pack(push, 2)
+typedef struct DLGTEMPLATEEX_t {
+    WORD dlgVer;
+    WORD signature;
+    DWORD helpID;
+    DWORD exStyle;
+    DWORD style;
+    WORD cDlgItems;
+    short x;
+    short y;
+    short cx;
+    short cy;
+} DLGTEMPLATEEX;
+#pragma pack(pop)
+
+static HGLOBAL get_dialog_with_font_size(HINSTANCE instance, LPCTSTR tmpl)
+{
+    int font_size = app_conf_get_int("DialogFontSize", 0, 0, 10);
+    if (!font_size)
+        return NULL;
+    HRSRC dialogRes = FindResource(instance, tmpl, RT_DIALOG);
+    DWORD resSize = SizeofResource(instance, dialogRes);
+    HGLOBAL mem_dialog = GlobalAlloc(GMEM_FIXED, resSize);
+    if (!mem_dialog)
+        goto bail;
+    HGLOBAL dialog = LoadResource(instance, dialogRes);
+    const VOID *dialog_p = LockResource(dialog);
+    if (!dialog_p)
+        goto bail;
+    DLGTEMPLATEEX *dialog_tmpl = (DLGTEMPLATEEX *)mem_dialog; // On Win32, GMEM_FIXED memory can be used as a pointer directly
+    memcpy(dialog_tmpl, dialog_p, resSize);
+    UnlockResource(dialog);
+    if (dialog_tmpl->signature == 0xffff && dialog_tmpl->style & DS_SETFONT) { // is EX and has font size and typeface
+        WORD *ptr = (WORD *)(dialog_tmpl + 1);
+        ptr = skip_dialog_x_array(ptr); // menu
+        ptr = skip_dialog_x_array(ptr); // class
+        ptr = skip_dialog_string(ptr); // title
+        *ptr = max(5, min(32, *ptr + font_size)); // font size
+    }
+    return mem_dialog;
+bail:
+    if (mem_dialog)
+        GlobalFree(mem_dialog);
+    return NULL;
+}
+
+static HWND CreateDialogWithFontSize(HINSTANCE hinst, LPCTSTR tmpl, HWND hwndparent, DLGPROC lpDialogFunc)
+{
+    HWND hwnd;
+    HGLOBAL dialog = get_dialog_with_font_size(hinst, tmpl);
+    if (dialog) {
+        hwnd = CreateDialogIndirect(hinst, dialog, hwndparent, lpDialogFunc);
+        GlobalFree(dialog);
+    } else {
+        hwnd = CreateDialog(hinst, tmpl, hwndparent, lpDialogFunc);
+    }
+    return hwnd;
 }

commit c4561a986e2869dac2acfa609edc054166c1c710

tools: fix referencing wrong ini section.
Broken in c7818e86e13e019e89112c286be3ff0f106ada14.

windows/pageant.c
@@ -40,7 +40,7 @@
 
 #define APPNAME "Pageant"
 
-const char *const appname = STR(APPNAME);
+const char *const appname = APPNAME;
 
 /* Titles and class names for invisible windows. IPCWINTITLE and
  * IPCCLASSNAME are critical to backwards compatibility: WM_COPYDATA

windows/puttygen.c
@@ -32,7 +32,7 @@
 
 #define APPNAME "PuTTYgen"
 
-const char *const appname = STR(APPNAME);
+const char *const appname = APPNAME;
 
 static Filename *cmdline_keyfile = NULL;
 static ptrlen cmdline_demo_keystr;