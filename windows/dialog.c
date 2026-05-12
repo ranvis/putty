19c199985db9abdb646307f265c7d56c2ec744af
SATO Kentaro <kentaro@ranvis.com>
2026-05-12 20:15:19 +0900

    etc: Add custom version notices.

--- windows/dialog.c
+++ windows/dialog.c
@@ -377,7 +377,10 @@ static INT_PTR CALLBACK AboutProc(HWND hwnd, UINT msg,
         char *text = dupprintf(
             "%s\r\n\r\n%s\r\n\r\n%s\r\n\r\n%s",
             appname, ver, buildinfo_text,
-            "\251 " SHORT_COPYRIGHT_DETAILS ". All rights reserved.");
+            "\251 " SHORT_COPYRIGHT_DETAILS ". All rights reserved."
+            "\r\n(This build is a modified version of the original software."
+            "\r\nIt is not affiliated with the original developers.)"
+        );
         sfree(buildinfo_text);
         SetDlgItemTextCp1252(hwnd, IDA_TEXT, text);
         MakeDlgItemBorderless(hwnd, IDA_TEXT);
